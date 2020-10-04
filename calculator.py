8b7d3196de50b3e6a1919e0ca993d70ca73703ba
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2020-10-04 00:21:02 -0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print("Addition: " + str(num1 + num2))
+print("Subtraction: " + str(num1 - num2))
+print("Multiplication" + str(num1 * num2))
+print("Division: " + str(num1 / num2))
+print("Power: " + str(num1 ** num2))
+
+
